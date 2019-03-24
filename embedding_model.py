Fix bug in weight computation for struc2vec

embedding_model.py
@@ -17,6 +17,9 @@ class EmbeddingModel(Model):
         # Walks is a placeholder holding the set of walks (B x L)
         walks = kwargs['walks']
 
+        # Tensor denoting nodes used for negative sampling (B x S)
+        neg_samples = kwargs['neg_samples']
+
         # Number of nodes in the graph
         num_nodes = kwargs['num_nodes']
 
@@ -34,6 +37,11 @@ class EmbeddingModel(Model):
                 walk_embeddings = tf.nn.embedding_lookup(node_embedding_var, walks, name='node-embeddings',
                                                          max_norm=1.0)
 
+                # Tensor Dims: B x S x D
+                neg_sample_embeddings = tf.nn.embedding_lookup(node_embedding_var, neg_samples,
+                                                               name='node-embeddings',
+                                                               max_norm=1.0)
+
                 # Tensor Dims: B x L x D
                 node_shape = tf.shape(node_embeddings)
                 node_expanded = tf.reshape(node_embeddings, [node_shape[0], 1, node_shape[1]])
@@ -42,13 +50,13 @@ class EmbeddingModel(Model):
                 # Tensor Dims: B x 1
                 neighborhood_sum = tf.reduce_sum(walk_multiply, axis=[1, 2])
 
+                # Tensor Dims: B x S
+                neg_sample_similarity = tf.reduce_sum(node_expanded * neg_sample_embeddings, axis=2)
+                neg_sample_sum = tf.reduce_sum(tf.exp(neg_sample_similarity), axis=1)
+
                 # Tensor Dims: B x B x D
                 edge_embeddings = node_expanded * tf.transpose(node_expanded, perm=[1, 0, 2])
 
-                # Tensor Dims: B x 1
-                node_dot_prod = tf.exp(tf.reduce_sum(edge_embeddings, axis=2))
-                all_nodes = tf.reduce_sum(node_dot_prod, axis=1)
-
-                self.loss_op = tf.reduce_sum(tf.log(all_nodes) - neighborhood_sum)
+                self.loss_op = tf.reduce_sum(tf.log(neg_sample_sum) - neighborhood_sum)
                 self.output_ops.append(node_embeddings)
                 self.output_ops.append(edge_embeddings)

graph_embedder.py
@@ -6,7 +6,7 @@ from os import mkdir
 from embedding_model import EmbeddingModel
 from random_walkers import RandomWalkerFactory
 from utils import create_mini_batches, append_to_log, create_walk_windows
-from utils import cluster_nodes, cluster_edges
+from utils import cluster_nodes, cluster_edges, create_negative_samples
 from plot import plot_graph
 from constants import *
 
@@ -31,9 +31,14 @@ class GraphEmbedder:
         # Create and initialize embedding model
         self.model = EmbeddingModel(params=params)
         self.nodes_ph = self.model.create_placeholder(dtype=tf.int32, shape=[None], name='nodes-ph')
-        self.walks_ph = self.model.create_placeholder(dtype=tf.int32, shape=[None, params['window_size']-1],
+        self.walks_ph = self.model.create_placeholder(dtype=tf.int32, shape=[None, params['window_size']],
                                                       name='walks-ph')
-        self.model.build(nodes=self.nodes_ph, walks=self.walks_ph, num_nodes=graph.number_of_nodes())
+        self.neg_samples_ph = self.model.create_placeholder(dtype=tf.int32, shape=[None, params['neg_samples']],
+                                                            name='neg-sample-ph')
+        self.model.build(nodes=self.nodes_ph,
+                         walks=self.walks_ph,
+                         neg_samples=self.neg_samples_ph,
+                         num_nodes=graph.number_of_nodes())
         self.model.init()
 
         # Make output folder for this model
@@ -47,23 +52,32 @@ class GraphEmbedder:
         append_to_log(['Epoch', 'Average Loss per Sample'], log_path)
 
         data_windows = create_walk_windows(walks=self.walks, window_size=self.params['window_size'])
-        print(len(data_windows[WALKS]))
+        
+        graph_nodes = list(self.graph.nodes())
+
         convergence_count = 0
         prev_loss = BIG_NUMBER
         for epoch in range(self.params['epochs']):
 
             # Create data batches
+            neg_samples = create_negative_samples(nodes=graph_nodes,
+                                                  walk_windows=data_windows[WALKS],
+                                                  node_windows=data_windows[NODES],
+                                                  num_neg_samples=self.params['neg_samples'])
             batches = create_mini_batches(walks=data_windows[WALKS],
                                           nodes=data_windows[NODES],
+                                          neg_samples=neg_samples,
                                           batch_size=self.params['batch_size'])
             walk_batches = batches[WALKS]
             node_batches = batches[NODES]
+            neg_sample_batches = batches[NEG_SAMPLES]
 
             losses = []
-            for walk_batch, node_batch in zip(walk_batches, node_batches):
+            for walk_batch, node_batch, neg_sample_batch in zip(walk_batches, node_batches, neg_sample_batches):
                 feed_dict = {
                     self.nodes_ph: node_batch,
-                    self.walks_ph: walk_batch
+                    self.walks_ph: walk_batch,
+                    self.neg_samples_ph: neg_sample_batch
                 }
                 loss = self.model.run_train_step(feed_dict)
                 losses.append(loss / float(len(walk_batch)))

random_walkers.py
@@ -89,10 +89,13 @@ class Struc2VecWalker(RandomWalker):
         degree_neighborhoods = {node: {} for node in graph.nodes()}
         self._add_kth_degree_neighborhood(graph, degree_neighborhoods, neighborhoods, 0)
 
-        # Initialize 0th level weights
-        l0_weight, l0_avg = self._compute_weights(graph, degree_neighborhoods, 0, None)
-        weights = [l0_weight]
-        avg_weights = [l0_avg]
+        # Initialize 0th level distances and weights
+        l0_dist = self._compute_distances(graph, degree_neighborhoods, 0, None)
+        distances = [l0_dist]
+
+        l0_weights = self._compute_weights(graph, l0_dist)
+        weights = [l0_weights]
+        avg_weights = [np.average(l0_weights)]
 
         nodes_lst = list(graph.nodes())
         walks = []
@@ -126,9 +129,13 @@ class Struc2VecWalker(RandomWalker):
                         if len(weights) <= k:
                             self._add_kth_neighborhood(graph, neighborhoods, k)
                             self._add_kth_degree_neighborhood(graph, degree_neighborhoods, neighborhoods, k)
-                            lk_weights, avg = self._compute_weights(graph, degree_neighborhoods, k, weights[k-1])
+                            
+                            lk_dist = self._compute_distances(graph, degree_neighborhoods, k, distances[k-1])
+                            lk_weights = self._compute_weights(graph, lk_dist)
+
+                            distances.append(lk_dist)
                             weights.append(lk_weights)
-                            avg_weights.append(avg)
+                            avg_weights.append(np.average(lk_weights))
 
                 walks.append(walk)
         return np.array(walks)
@@ -149,32 +156,32 @@ class Struc2VecWalker(RandomWalker):
         return degree_neighborhoods
 
     # O(n^2) algorithm to compute the weights for level k
-    def _compute_weights(self, graph, degree_neighborhoods, k, prev_layer_weights):
-        weights = []
-        total_weight = 0.0
+    def _compute_distances(self, graph, degree_neighborhoods, k, prev_layer_distances):
+        distances = []
         for u in graph.nodes():
-            w = [self._compute_weight(u, v, degree_neighborhoods, k, prev_layer_weights) \
+            d = [self._compute_distance(u, v, degree_neighborhoods, k, prev_layer_distances) \
                  for v in graph.nodes()]
-            w = np.exp(w)
+            distances.append(d)
 
-            # Prevent moving back to the same node
-            w[u] = 0.0
-            
-            # Normalize the weights
-            t = np.sum(w)
-            if t < SMALL_NUMBER:
-                w[u] = 1.0
-            else:
-                w = w / t
+        return np.array(distances)
 
-            total_weight += t
-            weights.append(w)
-
-        return np.array(weights), np.average(weights)
-
-    def _compute_weight(self, u, v, degree_neighborhoods, k, prev_layer_weights):
+    def _compute_distance(self, u, v, degree_neighborhoods, k, prev_layer_distances):
         if u == v:
             return 0.0
         distance, _ = fastdtw(degree_neighborhoods[u][k], degree_neighborhoods[v][k], dist=dist)
-        f_k = prev_layer_weights[u, v] + distance if prev_layer_weights is not None else distance
-        return -f_k
+        f_k = prev_layer_distances[u, v] + distance if prev_layer_distances is not None else distance
+        return f_k
+
+    def _compute_weights(self, graph, distances):
+        return [self._compute_weight(distances, n) for n in graph.nodes()]
+
+    def _compute_weight(self, distances, u):
+        distances = distances[u]
+        distances[u] = 0.0  # Prevent moving back to the same node
+        weights = np.exp(-distances)
+        s = np.sum(weights)
+        if s < SMALL_NUMBER:
+            weights[u] = 1.0
+        else:
+            weights = weights / s
+        return np.array(weights)

utils.py
@@ -6,20 +6,23 @@ from constants import *
 from sklearn.cluster import KMeans
 
 
-def create_mini_batches(walks, nodes, batch_size):
+def create_mini_batches(walks, nodes, neg_samples, batch_size):
     # Randomly shuffle data points
-    combined = list(zip(walks, nodes))
+    combined = list(zip(walks, nodes, neg_samples))
     np.random.shuffle(combined)
-    walks, nodes = zip(*combined)
+    walks, nodes, neg_samples = zip(*combined)
 
     walks_batches = []
     nodes_batches = []
+    neg_sample_batches = []
     for i in range(0, len(walks), batch_size):
         walks_batches.append(np.array(walks[i:i+batch_size]))
         nodes_batches.append(np.array(nodes[i:i+batch_size]))
+        neg_sample_batches.append(np.array(neg_samples[i:i+batch_size]))
     return {
         WALKS: np.array(walks_batches),
-        NODES: np.array(nodes_batches)
+        NODES: np.array(nodes_batches),
+        NEG_SAMPLES: np.array(neg_sample_batches)
     }
 
 
@@ -27,14 +30,30 @@ def create_walk_windows(walks, window_size):
     walk_windows = []
     node_windows = []
     for walk in walks:
-        for i in range(len(walk) - window_size):
-            window = walk[i:i+window_size]
+        for i in range(len(walk) - window_size - 1):
+            window = walk[i:i+window_size+1]
             walk_windows.append(window[1:])
             node_windows.append(window[0])
     return {
         WALKS: np.array(walk_windows),
         NODES: np.array(node_windows)
     }
+
+
+def create_negative_samples(nodes, walk_windows, node_windows, num_neg_samples):
+    neg_samples = []
+    for n, walk_window in zip(node_windows, walk_windows):
+        w = set(walk_window)
+        neg_sample = set()
+
+        # Create negative samples outside of the current context
+        while len(neg_sample) < num_neg_samples:
+            u = np.random.choice(nodes)
+            if (u != n) and (not u in w):
+                neg_sample.add(u)
+
+        neg_samples.append(list(neg_sample))
+    return np.array(neg_samples)
 
 
 def load_params(params_file_path):